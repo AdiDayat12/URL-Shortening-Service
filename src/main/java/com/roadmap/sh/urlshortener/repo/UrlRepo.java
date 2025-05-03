8d7f096ff0df6fd6382dde5dd2f78daa4afb422e
adiii <adilinan72@gmail.com>
2025-05-03 08:15:17 +0300

    create method findByShortCode to find an original link using a generated short code

--- src/main/java/com/roadmap/sh/urlshortener/repo/UrlRepo.java
+++ src/main/java/com/roadmap/sh/urlshortener/repo/UrlRepo.java
@@ -3,5 +3,8 @@ package com.roadmap.sh.urlshortener.repo;
 import com.roadmap.sh.urlshortener.entity.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UrlRepo extends JpaRepository<Url, Long> {
+    Optional<Url> findByShortCode(String shortCode);
 }
