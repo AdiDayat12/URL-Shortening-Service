ede4a84974ca5c4ed65cca7df6e37ff2c0e270e8
adiii <adilinan72@gmail.com>
2025-05-03 09:56:23 +0300

    create response class to handle every responses

---
+++ src/main/java/com/roadmap/sh/urlshortener/dto/ResponseAPI.java
@@ -0,0 +1,18 @@
+package com.roadmap.sh.urlshortener.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseAPI<T> {
+    private int code;
+    private String message;
+    private T data;
+}
